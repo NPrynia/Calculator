340af15700c7729812f02b1579d6f9a69a3fd984
Nikolay <Nikolay@KOLIAN>
2021-06-04 18:14:08 +0300

    Calculator

--- Lesson0106/Calculate.cs
+++ Lesson0106/Calculate.cs
@@ -3,39 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace Lesson0106
 {
     public class Calculate
     {
-        public static double calculate(double a, double b, int act)
+        public static double calculate(string expression)
         {
-            double res = 0;
-            switch (act)
+            double exp;
+            if (expression.Contains("^") == true)
             {
-                case 0:
-                    res = a + b;
+                String[] value = expression.Split(new char[] { '^' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    break;
-                case 1:
-                    res = a - b;
-                    break;
-                case 2:
-                    res = a * b;
+                DataTable table = new DataTable();
+                table.Columns.Add("expression", typeof(string), value[0]);
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                double val1 = double.Parse((string)row["expression"]);
 
-                    break;
-                case 3:
-                    res = a / b;
+                DataTable table1 = new DataTable();
+                table1.Columns.Add("expression", typeof(string), value[1]);
+                DataRow row1 = table1.NewRow();
+                table1.Rows.Add(row1);
+                double val2 = double.Parse((string)row1["expression"]);
 
-                    break;
-                case 4:
-                    res = Math.Pow(a, b);
 
-                    break;
+                exp =  Math.Pow(val1, val2);
 
             }
-            return res;
+            else
+            {
+                DataTable table = new DataTable();
+                table.Columns.Add("expression", typeof(string), expression);
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                exp = double.Parse((string)row["expression"]);
 
+            }
+
+
+            
+           
+            return exp;
+            
         }
 
     }
--- Lesson0106/MainWindow.xaml.cs
+++ Lesson0106/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Lesson0106;
+using System.Data;
+
+
 
 namespace Lesson0106
 {
@@ -20,19 +24,234 @@ namespace Lesson0106
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string exp;
+        
+       
         public MainWindow()
         {
             InitializeComponent();
         }
+        
 
-        private void btn_Click(object sender, RoutedEventArgs e)
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+
+
+        }
+
+        
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
         {
 
+            tbexp.Text = tbexp.Text + "1";
+        }
 
-            double a = Convert.ToDouble(tb1.Text);
-            double b = Convert.ToDouble(tb2.Text);
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "4";
+        }
 
-            tbres.Text = Convert.ToString(Calculate.calculate(a, b, Cb.SelectedIndex));
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "7";
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "0";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "2";
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "5";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "8";
+        }
+
+        private void btnComma_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (tbexp.Text != "")
+            {
+                string temp = tbexp.Text.Substring(tbexp.Text.Length - 1);
+                if (temp == "×" || temp == "^" || temp == "/" || temp == "-" || temp == "+" || temp == "%" || temp == ",") 
+                {
+
+
+
+                }
+                else
+                {
+                    tbexp.Text = tbexp.Text + ".";
+                }
+            }
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "3";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "6";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = tbexp.Text + "9";
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbexp.Text !=  "" )
+            {
+                char[] c = new char[tbexp.Text.Length - 1];
+                tbexp.Text.CopyTo(0, c, 0, c.Length);
+                tbexp.Text = new string(c);
+            }
+         
+        }
+
+        private void btnPow_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbexp.Text != "")
+            {
+                string temp = tbexp.Text.Substring(tbexp.Text.Length - 1);
+                if (temp == "*" || temp == "^" || temp == "/" || temp == "-" || temp == "+" || temp == "%" || temp == ",")
+                {
+
+
+
+                }
+                else
+                {
+                    tbexp.Text = tbexp.Text + "^";
+                }
+            }
+        }
+
+        private void btnMulti_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbexp.Text != "")
+            {
+                string temp = tbexp.Text.Substring(tbexp.Text.Length - 1);
+                if (temp == "*" || temp == "^" || temp == "/" || temp == "-" || temp == "+" || temp == "%" || temp == ",")
+                {
+
+
+
+                }
+                else
+                {
+                    tbexp.Text = tbexp.Text + "*";
+
+                }
+            }
+
+            
+            
+        }
+
+        private void btnDiv_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbexp.Text != "")
+            {
+                string temp = tbexp.Text.Substring(tbexp.Text.Length - 1);
+                if (temp == "*" || temp == "^" || temp == "/" || temp == "-" || temp == "+" || temp == "%" || temp == ",")
+                {
+
+
+
+                }
+                else
+                {
+                    tbexp.Text = tbexp.Text + "/";
+                }
+            }
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (tbexp.Text != "")
+            {
+                string temp = tbexp.Text.Substring(tbexp.Text.Length - 1);
+                if (temp == "*" || temp == "^" || temp == "/" || temp == "-" || temp == "+" || temp == "%" || temp == ",")
+                {
+
+
+
+                }
+                else
+                {
+                    tbexp.Text = tbexp.Text + "+";
+                }
+            }
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbexp.Text != "")
+            {
+                string temp = tbexp.Text.Substring(tbexp.Text.Length - 1);
+                if (temp == "*" || temp == "^" || temp == "/" || temp == "-" || temp == "+" || temp == "%" || temp == ",")
+                {
+
+
+
+                }
+                else
+                {
+                    tbexp.Text = tbexp.Text + "-";
+                }
+            }
+        }
+
+        private void btnProc_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbexp.Text != "")
+            {
+                string temp = tbexp.Text.Substring(tbexp.Text.Length - 1);
+            if (temp == "*" || temp == "^" || temp == "/" || temp == "-" || temp == "+" || temp == "%" || temp == ",")
+            {
+
+
+
+            }
+            else
+            {
+                tbexp.Text = tbexp.Text + "%";
+            }
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbexp.Text = "";
+        }
+
+        private void btnEq_Click(object sender, RoutedEventArgs e)
+        {
+            tbres.Text  = Convert.ToString(Calculate.calculate(tbexp.Text));
+            
+        }
+
+        private void btnPercnt_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
